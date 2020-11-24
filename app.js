e8bbc43189c5825cb77036c0ca66328dcf617eab
Per Kristian <kristiansvanberg@gmail.com>
2020-11-24 13:58:06 +0100

    added comment to the conditional statement

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
     // Adding a list item to the todo list
     $("#add-todo").click(function() {
 
+      // Checks if the user has added valid input
       if($("#todo-input").val() === "") {
         alert("No valid input added, please try again");
         return;
