b99a4ef39488981e8a72dfbe51ec091697723d68
Per Kristian <kristiansvanberg@gmail.com>
2020-11-24 13:36:23 +0100

    Added id's to html elements for working with jquery. Added functionality for adding a list item to the list

--- app.js
+++ app.js
@@ -4,7 +4,19 @@
 (function($) {
 
   $(document).ready(function() {
-    console.log("DOCUMENT LOADED");
-  })
+    $("#add-todo").click(function() {
+      // getting the todo-list
+      $list = $("#todo-list");
 
-})(jQuery)
+      // getting the user input
+      $input = $("#todo-input").val();
+
+      // creating a list item
+      $todo = $('<li><i class="fas fa-check"></i>' + $input + '<i class="fas fa-times"></i></li>');
+
+      // append list item to the list
+      $list.append($todo);
+    });
+  });
+  
+})(jQuery);
